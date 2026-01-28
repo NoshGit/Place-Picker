dc3b112237ff437d97d66a86d0f31b306262acd6
Noshir Patel <noshirpatel86@gmail.com>
2026-01-28 19:31:49 +0530

    Refactor AvailablePlaces and Places components to handle loading state and improve user feedback (using async await)

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -2,18 +2,27 @@ import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isLoading, setIsLoading] = useState(false);
   const [places, setPlaces] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3000/places')
-      .then((response) => response.json())
-      .then((data) => setPlaces(data.places));
+    async function fetchPlaces() {
+      setIsLoading(true);
+      const response = await fetch('http://localhost:3000/places');
+      const data = await response.json();
+      setPlaces(data.places);
+      setIsLoading(false);
+    }
+
+    fetchPlaces();
   }, []);
 
   return (
     <Places
       title="Available Places"
       places={places}
+      isLoading={isLoading}
+      loadingText="Fetching places..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -1,12 +1,22 @@
 import { log } from './log.js';
 
-export default function Places({ title, places, fallbackText, onSelectPlace }) {
+export default function Places({
+  title,
+  places,
+  fallbackText,
+  onSelectPlace,
+  isLoading,
+  loadingText,
+}) {
   log(places);
   return (
     <section className="places-category">
       <h2>{title}</h2>
-      {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
-      {places.length > 0 && (
+      {isLoading && <p className="loading-text">{loadingText}</p>}
+      {!isLoading && places.length === 0 && (
+        <p className="fallback-text">{fallbackText}</p>
+      )}
+      {!isLoading && places.length > 0 && (
         <ul className="places">
           {places.map((place) => (
             <li key={place.id} className="place-item">
