45fddd978d584af003d8fcd4cf98f843983b86a1
Noshir Patel <noshirpatel86@gmail.com>
2026-01-28 18:30:26 +0530

    Fetch and display available places in AvailablePlaces component

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,19 @@
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then((response) => response.json())
+      .then((data) => setPlaces(data.places));
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={places}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
